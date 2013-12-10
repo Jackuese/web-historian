35e5695db403a9546d1d773f0cbcd599e4cd2ab6
Hack Reactor Students <jackson.clowes@gmail.com>
2013-12-09 17:24:11 -0800

    Early commit

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var fs = require('fs');
+var url = require('url');
 
 exports.headers = headers = {
   "access-control-allow-origin": "*",
@@ -10,8 +11,32 @@ exports.headers = headers = {
 };
 
 exports.serveStaticAssets = function(res, folder, asset) {
+  firstRun(res,folder);
   //Write some code here that helps serve up your static files!
-  //(Static files are things like html (yours or arhived from others...), css, or anything that doesn't change often.)
+  //(Static files are things like html (yours or ones archived from others...), css, or anything that doesn't change often.)
 };
 
+var firstRun = function(res, ur) {
+  if(ur === '/') {
+    res.writeHead(200, headers);
+    var output = '';
+    fs.readFile('./web/public/index.html', 'utf8', function (err, data){
+      output += data;
+      res.end(output);
+    });
+    } else {
+      res.writeHead(200, headers);
+      fs.readFile('./data/sites' + ur, 'utf8', function (err, data){
+        output += data;
+        console.log("This is Google" + data);
+        res.end(output);
+      });
+    }
+};
+
+var readArchive = function (res, ur){
+  // look at the file containing the list of urls (./data/sites.txt)
+  // if it finds an address matching the queried site (/www.google.com)
+  // it should go through the site archive at ./data/sites and send the html for the chosen site back
+};
 // As you go through, keep thinking about what helper functions you can put here!
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,6 +1,15 @@
 var path = require('path');
+var helpers = require('./http-helpers');
+var file = require('fs');
+var url = require('url');
+
 module.exports.datadir = path.join(__dirname, "../data/sites.txt"); // tests will need to override this.
+var firstTime = true;
 
 module.exports.handleRequest = function (req, res) {
   console.log(exports.datadir);
+  var parsedURL = url.parse(req.url);
+  console.log(parsedURL.pathname);
+  helpers.serveStaticAssets(res, parsedURL.pathname);
 };
+
